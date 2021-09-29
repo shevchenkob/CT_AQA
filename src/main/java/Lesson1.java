ce01f4ba06cf64b5235e0b42c2466a72d4e2a2f1
shevchenkob <shevbv@gmail.com>
2021-09-29 14:45:27 +0300

    1

--- src/main/java/Lesson1.java
+++ src/main/java/Lesson1.java
@@ -1,13 +1,26 @@
 public class Lesson1 {
 
+    /*
+    In this method we add wrapper to integer data type and compering input value with 11 number.
+     */
     public static void wrapperInt () {
         int inputInteger = 10;
         int transformInt = Integer.compare(inputInteger, 11);
         System.out.println(transformInt);
     }
 
+    /*
+    In this method we add wrapper to char and changing input value to uppercase.
+    */
+    public static void wrapperChar () {
+        char inputChar = 'l';
+        char transformChar = Character.toUpperCase(inputChar);
+        System.out.println(transformChar);
+
+    }
+
     public static void main(String[] args) {
-        wrapperInt();
+        wrapperChar();
     }
 
 }
