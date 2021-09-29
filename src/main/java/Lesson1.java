3aaac1ceef3eaf1e76e8086fba2ad72a0d455ace
shevchenkob <shevbv@gmail.com>
2021-09-29 19:18:48 +0300

    1

--- src/main/java/Lesson1.java
+++ src/main/java/Lesson1.java
@@ -1,3 +1,5 @@
+import java.nio.charset.StandardCharsets;
+
 public class Lesson1 {
 
     /*
@@ -19,8 +21,37 @@ public class Lesson1 {
 
     }
 
+    /*
+    In method below we add wrapper to short data type and getting 3-rd char after transforming to string type.
+     */
+    public static void wrapperShort () {
+        short inputShort = 12354;
+        char transformedShort = Short.toString(inputShort).charAt(2);
+        System.out.println(transformedShort);
+
+    }
+
+    /*
+    In this method we add wrapper to long data type and replaising digit 3 to "a" symbol after converting to string.
+    */
+    public static void wrapperLong () {
+        long inputLong = 1236534665;
+        String transformLong = Long.toString(inputLong).replace('3', 'a');
+        System.out.println(transformLong);
+
+    }
+
+    /*
+    In this method we add wrapper to byte data type and after converting to string get first position of digit.
+    */
+    public static void wrapperByte () {
+        byte inputByte = 21;
+        String transformByte = Byte.toString(inputByte);
+        System.out.println(transformByte.charAt(0));
+    }
+
     public static void main(String[] args) {
-        wrapperChar();
+        wrapperByte();
     }
 
 }
