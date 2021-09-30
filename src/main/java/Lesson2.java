ed2055a070868fb165ced6a6d9f4b68ad968cc2d
shevchenkob <shevbv@gmail.com>
2021-10-01 01:08:29 +0300

    1

---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,19 @@
+public class Lesson2 {
+
+        public static void modulus(){
+            int num1,num2,result;
+            num1=36;
+            num2=15;
+            System.out.println("num1= " + num1 + " num2= " + num2);
+            result=num1%num2;
+            System.out.println("The result after modulus operation is : "+result);
+
+        }
+
+
+
+    public static void main(String[] args){
+            modulus();
+    }
+
+}
