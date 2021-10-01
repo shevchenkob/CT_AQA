2dc45f995dd4df9d3d8fffb636d849baa5fe7a66
shevchenkob <shevbv@gmail.com>
2021-10-01 22:47:11 +0300

    1

--- src/main/java/Lesson2.java
+++ src/main/java/Lesson2.java
@@ -1,11 +1,35 @@
 public class Lesson2 {
-    public static void modulus(){
+    public static void modulusAndOther(){
         int num1,num2,result;
         num1=36;
         num2=15;
         System.out.println("num1= " + num1 + " num2= " + num2);
         result=num1%num2;
         System.out.println("The result after modulus operation is : "+result);
+
+                /* Increment */
+        /*
+        Here we first add 1, so b=8 and then "a" became "b", so "a" also 8.
+        */
+        int a,b = 7;
+        a = ++b;
+        System.out.println("a = " + a + " b = " + b);
+
+        /*
+        Here we first make "c" became "c", so "a" = c and then add 1 to "d", so d=8.
+        */
+        int c,d = 7;
+        c = d++;
+        System.out.println("c = " + c + " d = " + d);
+
+                    /* Decrement */
+
+        /* in "q" we first subtract 1 from "e" and then add to 10, in t we first add "f" to five and then subtract 1. */
+        int e = 5;
+        int f = 5;
+        int q = 10 + --e;
+        int t = 10 + f--;
+        System.out.println("q is: " + q + "t is: " + t);
 
     }
 
@@ -41,7 +65,7 @@ public class Lesson2 {
 
 
     public static void main(String[] args){
-        forMethod();
+        modulusAndOther();
     }
 
 }
