dc3911921bf2aa021b7a97aaff76cae9c0299e04
shevchenkob <shevbv@gmail.com>
2021-10-06 18:02:19 +0300

    1

--- src/main/java/com/customertimes/Lesson3/Lesson3personaltask.java
+++ src/main/java/com/customertimes/Lesson3/Lesson3personaltask.java
@@ -6,7 +6,17 @@ public class Lesson3personaltask {
 
     public static void avverageInArray (){
         int[] myArray = {1, 2, 3, 99, 4};
-        Arrays.sort(myArray);
+        //Arrays.sort(myArray);
+
+        for (int i = 0; i<myArray.length; i++) {
+            for (int j = i+1; j<myArray.length; j++) {
+                if (myArray[i]<myArray[j]) {
+                   int temp = myArray[i];
+                    myArray[i]=myArray[j];
+                    myArray[j]=temp;
+                }
+            }
+        }
         int firstNum = myArray[0];
         int lastNum = myArray[myArray.length-1];
         int avgSum = (firstNum + lastNum)/2;
