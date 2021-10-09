c1a6bf6325456b239ac398c117c23a6f7d31a2dc
shevchenkob <shevbv@gmail.com>
2021-10-09 23:59:24 +0300

    1

---
+++ src/main/java/com/customertimes/Lesson3/MyEnum/Calculator_NOT_DONE_YET.java
@@ -0,0 +1,32 @@
+package com.customertimes.Lesson3.MyEnum;
+
+import static com.customertimes.Lesson3.MyEnum.EnumCalculator_NOT_DONE_YET.PLUSoperation;
+
+public class Calculator_NOT_DONE_YET {
+    public int calculate(String[] args) {
+
+        PLUSoperation {
+            public int calc ( int x, int y){
+                return x + y;
+            }
+        },
+        MINUSoperation {
+            public int calc ( int x, int y){
+                return x - y;
+            }
+        },
+        TIMESoperation {
+            public int calc ( int x, int y){
+                return x * y;
+            }
+        },
+        DIVISIONoperation {
+            public int calc ( int x, int y){
+                return x / y;
+            }
+        }
+    }
+
+}
+
+
---
+++ src/main/java/com/customertimes/Lesson3/MyEnum/EnumCalculator_NOT_DONE_YET.java
@@ -0,0 +1,9 @@
+package com.customertimes.Lesson3.MyEnum;
+
+public enum EnumCalculator_NOT_DONE_YET {
+    PLUSoperation,
+    MINUSoperation,
+    TIMESoperation,
+    DIVISIONoperation
+
+}
