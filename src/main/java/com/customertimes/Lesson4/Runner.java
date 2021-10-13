3f83f4204782b002ca77094bdc32dee864207f41
shevchenkob <shevbv@gmail.com>
2021-10-14 01:18:02 +0300

    1

--- src/main/java/com/customertimes/Lesson4/Runner.java
+++ src/main/java/com/customertimes/Lesson4/Runner.java
@@ -1,27 +1,30 @@
 package com.customertimes.Lesson4;
 
+import com.customertimes.Lesson4.PersonalsEnums.DoctorsEnum;
+import com.customertimes.Lesson4.PersonalsEnums.NurseEnum;
+
 public class Runner {
 
     public static void main(String... args) {
-        Surgeon surgeon = new Surgeon();
+        Surgeon surgeon = new Surgeon(10, 1980, "Ivan", "emergency", 50, DoctorsEnum.FIRST_RANK);
         surgeon.work();
 
-        Therapist therapist = new Therapist();
+        Therapist therapist = new Therapist(15, 1975, "Vasya", "usual", 40, DoctorsEnum.HIGHEST_RANK);
         therapist.work();
 
-        Nurses nurses = new Nurses();
+        Nurses nurses = new Nurses(8, 1995, "Kate", "profilact", NurseEnum.SECOND_CATEGORY);
         nurses.work();
 
-        Administrators administrators= new Administrators();
+        Administrators administrators= new Administrators(5, 2000, "Ann", "vaccination", "reception");
         administrators.work();
 
-        Accountant accountant = new Accountant();
+        Accountant accountant = new Accountant(52, 1992, "John", "protection", "sighting");
         accountant.work();
 
-        Pharmacist pharmacist = new Pharmacist();
+        Pharmacist pharmacist = new Pharmacist(37, 1988, "Sara", "giveAway", "aspirin");
         pharmacist.work();
 
-        Personal newNurse = new Nurses();
+        Personal newNurse = new Nurses(22, 2001, "Megan", "intensiveCare", NurseEnum.FIRST_CATEGORY);
         System.out.println(newNurse.name);
     }
 }
