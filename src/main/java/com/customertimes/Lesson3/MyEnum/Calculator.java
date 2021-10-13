d10f4e1c4796475af219ac2b4ab669eedad1c5b6
shevchenkob <shevbv@gmail.com>
2021-10-13 18:32:21 +0300

    1

--- src/main/java/com/customertimes/Lesson3/MyEnum/Calculator.java
+++ src/main/java/com/customertimes/Lesson3/MyEnum/Calculator.java
@@ -3,30 +3,33 @@ package com.customertimes.Lesson3.MyEnum;
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println(calculate(EnumCalculator.PLUS, 3, 0, 5));
+        System.out.println(calculate(EnumCalculator.PLUS, 3, 1, 5));
     }
 
-    public static double calculate
-            (EnumCalculator operation, int i, int[] j) {
-        for (double digits : j) {
+    public static double calculate (EnumCalculator operation, int... i) {
+        int j = i[0];
+        for (int k=1; k<i.length; k++) {
             switch (operation) {
                 case PLUS:
-                    return (i + digits);
+                     j += i[k];
+                     break;
                 case MINUS:
-                    return (i - digits);
+                    j -= i[k];
+                    break;
                 case TIMES:
-                    return (i * digits);
+                    j *= i[k];
+                    break;
                 case DIVISION:
-                    if (digits == 0){
+                    if (j == 0){
                         System.out.println("It is protected to division by 0.");
                         break;
                     } else {
-                        return (i / digits);
-                    }
+                        j /= i[k];
 
+                    }
             }
         }
-        return i;
+        return j;
     }
 }
 
