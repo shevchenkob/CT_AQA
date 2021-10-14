61d45d7158ee5a060efb4e92d7795656dbc88c78
shevchenkob <shevbv@gmail.com>
2021-10-14 18:15:45 +0300

    1

--- src/main/java/com/customertimes/Lesson5/DateTime.java
+++ src/main/java/com/customertimes/Lesson5/DateTime.java
@@ -1,21 +1,34 @@
 package com.customertimes.Lesson5;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 public class DateTime {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         ZoneId zoneIdLosAngeles = ZoneId.of("America/Los_Angeles");
         LocalDateTime current = LocalDateTime.now(zoneIdLosAngeles);
-        System.out.println("Before formatting: " + current);
+        //System.out.println("Before formatting: " + current);
 
         DateTimeFormatter formatCurrent = DateTimeFormatter.ofPattern("YYYY/MMMM/EEEE HH:mm");
         String formatted = current.format(formatCurrent);
         System.out.println("Date and time after formatting: " + formatted + " " + zoneIdLosAngeles);
+
+        String myTime = "1986-22-08 12:30:1";
+        //DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-dd-MM HH:mm:ss");
+        //LocalDateTime dateTime = LocalDateTime.parse(myTime, formatter);
+
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+        Date date = inputFormat.parse(myTime);
+        System.out.println("Formatted from string: " + date);
     }
 }
---
+++ src/main/java/com/customertimes/Lesson5/StringWork.java
@@ -0,0 +1,10 @@
+package com.customertimes.Lesson5;
+
+public class StringWork {
+
+   public static String toOneText = "In this case, \"Hello world!\\/\\\\\" is a string literal—a series of characters in your code that is enclosed in double quotes. Whenever it encounters a string literal in your code, the compiler creates a String object with its value—in this case, Hello world\\.\n As with any\\other object, you can create String objects by using the new keyword and a constructor. The String class has thirteen constructors that allow you to provide the initial value of the string using different sources, such as an array of characters\n";
+
+    public static void main(String[] args) {
+        System.out.println(toOneText);
+    }
+}
