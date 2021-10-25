8ed790f97e602ca4c63d794742104535f7e35706
shevchenkob <shevbv@gmail.com>
2021-10-25 23:20:27 +0300

    1

--- src/main/java/com/customertimes/Lesson7/myRegexp.java
+++ src/main/java/com/customertimes/Lesson7/myRegexp.java
@@ -10,8 +10,8 @@ public class myRegexp {
     }
 
     private static void regExpression1() {
-        Pattern pattern = Pattern.compile("^[User ][Anatolii98|Sergeid][updated the page at ][0-9]{1-2}[:][0-9]{2}[,][0-9]{1-4}[am|pm][ days ago]");
-        Matcher matcher = pattern.matcher("User Anatolii98 updated the page at 11:11, 5 days ago");
+        Pattern pattern = Pattern.compile("^User (?:Anatolii98|Sergei) updated the page at \\d{1,2}:\\d{2} (?:am|pm), \\d+ days ago$");
+        Matcher matcher = pattern.matcher("User Anatolii98 updated the page at 11:11 am, 5 days ago");
         boolean isFound = matcher.find();
         String result = isFound ? "Match found" : "Match not found";
         System.out.println(result);
