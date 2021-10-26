0df6911cd4967656f14146fff11a363eec1858e0
shevchenkob <shevbv@gmail.com>
2021-10-26 23:38:42 +0300

    1

--- src/main/java/com/customertimes/Lesson8/MyCollections.java
+++ src/main/java/com/customertimes/Lesson8/MyCollections.java
@@ -1,17 +1,18 @@
 package com.customertimes.Lesson8;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyCollections {
 
     public static void main(String... args) {
-        myList();
+        // myArrayList();
+        myLinkedList();
 
     }
 
-    public static void myList() {
+    public static void myArrayList() {
         List<String> myArray = new ArrayList<>();
         myArray.add(0, "Volvo");
         myArray.add(1, "Tesla");
@@ -27,5 +28,28 @@ public class MyCollections {
         for (int i = 0; i < myArray.size(); i++) {
             System.out.println(myArray.get(i) + "_" + i);
         }
+    }
+
+    public static void myLinkedList() {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Dog");
+        list.add("Cat");
+        list.add("Mouse");
+        list.add("Bear");
+        list.add("Rat");
+        list.add("Cow");
+        list.add("Horse");
+        list.add("Pig");
+        list.add("Rabbit");
+        list.add("Bull");
+
+        System.out.println("List before removing: " + list);
+
+        list.remove(2);
+        list.remove(3);
+        list.remove(5);
+
+        System.out.println("List after removing: " + list);
+
     }
 }
