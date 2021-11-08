a651c9b1e630a198909aea7ae497109c4b354fef
shevchenkob <shevbv@gmail.com>
2021-11-08 23:58:40 +0200

    1

--- src/main/java/com/customertimes/Lesson9/NursesModel.java
+++ src/main/java/com/customertimes/Lesson9/NursesModel.java
@@ -2,9 +2,13 @@ package com.customertimes.Lesson9;
 
 import com.customertimes.Lesson4.personals_enums.NurseEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 public class NursesModel {
 
+    @Getter
+    @Setter
     @JsonProperty("category")
     public NurseEnum category;
 
@@ -15,14 +19,14 @@ public class NursesModel {
     public NursesModel() {
 
     }
-
-    public NurseEnum getCategory() {
-        return category;
-    }
-
-    public void setCategory(NurseEnum category) {
-        this.category = category;
-    }
+//
+//    public NurseEnum getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(NurseEnum category) {
+//        this.category = category;
+//    }
 
 
 }
