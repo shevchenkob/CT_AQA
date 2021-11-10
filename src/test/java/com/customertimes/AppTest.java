28805525079d48fd9fc5bdc8e8277e103d0b172c
shevchenkob <shevbv@gmail.com>
2021-11-11 01:20:16 +0200

    1

---
+++ src/test/java/com/customertimes/AppTest.java
@@ -0,0 +1,50 @@
+package com.customertimes;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class AppTest extends BaseTest{
+
+    @Test (description = "testOne")
+    public void testOne () throws InterruptedException {
+        driver.get("https://www.google.com/");
+        Thread.sleep(3_000);
+        String googleTitle = driver.getTitle();
+        Assert.assertEquals(googleTitle, "Google");
+
+    }
+
+    @Test (description = "testTwo")
+    public void testTwo () throws InterruptedException {
+        driver.get("https://github.com/");
+        Thread.sleep(3_000);
+        String gitHubTitle = driver.getTitle();
+        Assert.assertEquals(gitHubTitle, "GitHub: Where the world builds software · GitHub");
+
+    }
+
+    @Test (description = "testThree")
+    public void testThree () throws InterruptedException {
+        driver.get("https://www.salesforce.com/");
+        Thread.sleep(3_000);
+       String SFtitle = driver.getTitle();
+       Assert.assertEquals(SFtitle, "Salesforce: We Bring Companies and Customers Together");
+
+
+    }
+
+    @Test (description = "Random generation one")
+    public void testRandomOne () {
+        Random rand = new Random();
+        int upperbound = 10000;
+        int int_rand = rand.nextInt(upperbound);
+        int length = String.valueOf(int_rand).length();
+        Assert.assertEquals(length, 4);
+        System.out.println(int_rand);
+
+    }
+}
---
+++ src/test/java/com/customertimes/BaseTest.java
@@ -0,0 +1,21 @@
+package com.customertimes;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuit() {
+        driver = WebDriverManager.chromiumdriver().create();
+    }
+
+    @AfterSuite
+    public void afterSuit() {
+        driver.quit();
+    }
+
+}
