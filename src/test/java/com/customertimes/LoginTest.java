01a1907d54a80d9007bc1332bb0d611e79303830
shevchenkob <shevbv@gmail.com>
2021-11-23 01:47:55 +0200

    1

--- src/test/java/com/customertimes/LoginTest.java
+++ src/test/java/com/customertimes/LoginTest.java
@@ -10,10 +10,7 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-
-
-
-    @Test (description = "loginOne")
+    @Test(description = "loginOne")
     public void loginOne() throws InterruptedException {
 
         Faker faker = new Faker();
@@ -21,18 +18,43 @@ public class LoginTest extends BaseTest {
         String password = faker.internet().password();
 
         driver.get("https://www.linkedin.com/");
-       WebElement loginField = driver.findElement(By.id("session_key"));
-       loginField.sendKeys(email);
-       WebElement passwordField = driver.findElement(By.id("session_password"));
-       passwordField.sendKeys(password);
-       WebElement submitButton = driver.findElement(By.className("sign-in-form__submit-button"));
-       submitButton.click();
+        WebElement loginField = driver.findElement(By.id("session_key"));
+        loginField.sendKeys(email);
+        WebElement passwordField = driver.findElement(By.id("session_password"));
+        passwordField.sendKeys(password);
+        WebElement submitButton = driver.findElement(By.className("sign-in-form__submit-button"));
+        submitButton.click();
         Thread.sleep(2_000);
-        Assert.assertTrue(isPageLoaded(), "Negative login test failed");
+        Assert.assertTrue(isLinkedinCheckYouPageLoaded(), "Negative login test failed");
 
     }
 
-    public boolean isPageLoaded() {
-        return  driver.getCurrentUrl().contains("https://www.linkedin.com/checkpoint/challenge/");
+    public boolean isLinkedinCheckYouPageLoaded() {
+        return driver.getCurrentUrl().contains("https://www.linkedin.com/checkpoint/challenge/");
+    }
+
+    @Test
+    public void loginTwo() throws InterruptedException {
+
+        Faker faker = new Faker();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password();
+
+        driver.get("https://test.salesforce.com/");
+        WebElement loginFieldSF = driver.findElement(By.cssSelector("#username"));
+        loginFieldSF.sendKeys(email);
+        WebElement passwordFieldSF = driver.findElement(By.cssSelector("#password"));
+        passwordFieldSF.sendKeys(password);
+        WebElement submitLoginSF = driver.findElement(By.cssSelector("#Login"));
+        submitLoginSF.click();
+        Thread.sleep(20_000);
+        WebElement errorMessageSalesforceLogin = driver.findElement(By.cssSelector("#error"));
+        Assert.assertEquals(true, errorMessageSalesforceLogin.isDisplayed());
+
+    }
+
+    public boolean wrongSfLogin() {
+        WebElement errorMessageSalesforceLogin = driver.findElement(By.cssSelector("#error"));
+        return driver.equals(errorMessageSalesforceLogin);
     }
 }
