e449b3242b4d00fb391a6f2bd63f589ae0a94ea6
shevchenkob <shevbv@gmail.com>
2021-12-06 22:53:43 +0200

    1

--- src/test/java/com/customertimes/Tests/ShopTest21Vek.java
+++ src/test/java/com/customertimes/Tests/ShopTest21Vek.java
@@ -2,18 +2,28 @@ package com.customertimes.Tests;
 
 import com.customertimes.Pages.OneProductPage21Vek;
 import com.customertimes.Pages.ProductsPage21Vek;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ShopTest21Vek extends BaseTest {
 
     @Test(priority = 1)
-    public void successfulLoginTest() {
+    public void successfulLoginTest() throws IOException {
         ProductsPage21Vek productsPage21Vek = new ProductsPage21Vek(driver);
         productsPage21Vek.navigateToSecondPage();
         Assert.assertTrue(productsPage21Vek.isPageLoaded(), "Second page not loaded");
         productsPage21Vek.shopFridge();
         OneProductPage21Vek oneProductPage21Vek = new OneProductPage21Vek(driver);
+
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+
         Assert.assertTrue(oneProductPage21Vek.isPageLoaded());
         oneProductPage21Vek.buyFridge();
 
